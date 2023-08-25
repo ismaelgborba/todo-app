da5a62182e734561c054d6fc8abee199d0404740
Ismael Borba <ismael.borba@ab-inbev.com>
2023-08-25 13:58:14 -0300

    feat: authenticated middleware

---
+++ backend/src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const auth_header = request.headers.authorization;
+
+  if (!auth_header) {
+    throw new Error ("Token missing.");
+  }
+
+  const [_, token] = auth_header.split(" ");
+
+  try {
+    const { sub: user_id } = verify(token, 'express') as IPayload;
+
+    request.user = {
+      id: user_id
+    }
+
+    next();
+  } catch {
+    throw new Error("Invalid token.");
+  }
+}

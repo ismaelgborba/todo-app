7a6b30b438ab146aa1b503cbe2a75cdad929b7a1
Ismael Borba <ismael.borba@ab-inbev.com>
2023-08-25 14:46:36 -0300

    feat: add list all tasks and add relation user to task

--- backend/src/shared/infra/http/routes/tasks.routes.ts
+++ backend/src/shared/infra/http/routes/tasks.routes.ts
@@ -3,15 +3,18 @@ import { Router } from "express";
 import { CompleteTaskController } from "../../../../modules/tasks/use-cases/CompleteTask/CompleteTaskController";
 import { CreateTaskController } from "../../../../modules/tasks/use-cases/CreateTask/CreateTaskController";
 import { DeleteTaskController } from "../../../../modules/tasks/use-cases/DeleteTask/DeleteTaskController";
+import { ListAllTasksController } from "../../../../modules/tasks/use-cases/ListAllTasks/ListAllTasksController";
 
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
+const listAllTasksController = new ListAllTasksController();
 const createTaskController = new CreateTaskController();
 const deleteTaskController = new DeleteTaskController();
 const completeTaskController = new CompleteTaskController();
 
 const tasksRoutes = Router();
 
+tasksRoutes.get("/", ensureAuthenticated, listAllTasksController.handle);
 tasksRoutes.post("/", ensureAuthenticated, createTaskController.handle);
 tasksRoutes.delete("/:id", ensureAuthenticated, deleteTaskController.handle);
 tasksRoutes.put("/:id", ensureAuthenticated, completeTaskController.handle);

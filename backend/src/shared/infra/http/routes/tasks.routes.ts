3885572678be76927df1c9b25fabf6d141b64c6c
Ismael Borba <ismael.borba@ab-inbev.com>
2023-08-25 14:09:17 -0300

    feat: delete task

--- backend/src/shared/infra/http/routes/tasks.routes.ts
+++ backend/src/shared/infra/http/routes/tasks.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { CreateTaskController } from "../../../../modules/tasks/use-cases/CreateTask/CreateTaskController";
+import { DeleteTaskController } from "../../../../modules/tasks/use-cases/DeleteTask/DeleteTaskController";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const createTaskController = new CreateTaskController();
+const deleteTaskController = new DeleteTaskController();
 
 const tasksRoutes = Router();
 
-tasksRoutes.post("/", createTaskController.handle);
+tasksRoutes.post("/", ensureAuthenticated, createTaskController.handle);
+tasksRoutes.delete("/", ensureAuthenticated, deleteTaskController.handle)
 
 export { tasksRoutes };
